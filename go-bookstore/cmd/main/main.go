4a6affe9b355028d0799e66fb81ed8cf1a137561
Zahid <zadahmed@outlook.com>
2022-04-19 15:05:15 +0400

    Completed Go Bookstore

---
+++ go-bookstore/cmd/main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/zadahmed/go-bookstore/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":9010", r))
+}
